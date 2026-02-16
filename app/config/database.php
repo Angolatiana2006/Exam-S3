39a8957c85cbfd20d8e39d0ed2ab7db0d60ad3ae
laureen28 <laureenrazafindrabe@gmail.com>
2026-02-16 15:04:46 +0300

    database.php

---
+++ app/config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database {
+
+    private static $pdo;
+
+    public static function connect(){
+
+        if(!isset(self::$pdo)){
+            self::$pdo = new PDO(
+                "mysql:host=localhost;dbname=bngrc;charset=utf8",
+                "root",
+                ""
+            );
+
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        return self::$pdo;
+    }
+}
