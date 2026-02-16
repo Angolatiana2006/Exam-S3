8a324c6635d4d705e61f8f017a7fab75ed094453
tahiry <tahiryaritratiana@gmail.com>
2026-02-16 15:32:05 +0200

    DonController

---
+++ app/controllers/DonController.php
@@ -0,0 +1,17 @@
+<?php
+include 'DonModel.php';
+include 'database.php'; // fichier de connexion à la base
+
+$model = new DonModel($conn);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $type = $_POST['besoin_type_id'];
+    $quantite = $_POST['quantite'];
+
+    if ($model->ajouterDon($type, $quantite)) {
+        header("Location: dons.html?success=1");
+    } else {
+        header("Location: dons.html?error=1");
+    }
+}
+?>
